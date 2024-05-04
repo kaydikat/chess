88f5b10c247096c35a6f3f804e3414f999bb16f6
Kaydi <@kaydancegillam@gmail.com>
2024-05-04 13:18:46 -0600

    Made so Rook can recognize pieces. Now I just need to make it kill the opposite color.

--- shared/src/main/java/chess/RookMovesCalculator.java
+++ shared/src/main/java/chess/RookMovesCalculator.java
@@ -7,17 +7,47 @@ public class RookMovesCalculator {
   public Collection<ChessMove> calculateMoves(ChessBoard board, ChessPosition myPosition, ChessGame.TeamColor pieceColor) {
     var moves = new ArrayList<ChessMove>();
     System.out.println("myPosition: " + myPosition.getRow() + " " + myPosition.getColumn());
-    for (int i = 1; i <= 8; i++) {
-      if (i != myPosition.getRow()) {
+
+    // Iterate from the current row to row 1
+    for (int i = myPosition.getRow() - 1; i >= 1; i--) {
+      ChessPiece piece = board.getPiece(new ChessPosition(i, myPosition.getColumn()));
+      if (piece == null || piece.getTeamColor() != pieceColor) {
         moves.add(new ChessMove(myPosition, new ChessPosition(i, myPosition.getColumn()), null));
+      } else {
+        break; // Stop iteration if blocked by a piece of the opposite color
       }
     }
-    for (int i = 1; i <= 8; i++) {
-      if (i != myPosition.getColumn()) {
-        moves.add(new ChessMove(myPosition, new ChessPosition(myPosition.getRow(), i), null));;
+
+    // Iterate from the current row to row 8
+    for (int i = myPosition.getRow() + 1; i <= 8; i++) {
+      ChessPiece piece = board.getPiece(new ChessPosition(i, myPosition.getColumn()));
+      if (piece == null || piece.getTeamColor() != pieceColor) {
+        moves.add(new ChessMove(myPosition, new ChessPosition(i, myPosition.getColumn()), null));
+      } else {
+        break; // Stop iteration if blocked by a piece of the opposite color
       }
     }
-    //moves.add(new ChessMove(myPosition, new ChessPosition(2, 5), null));
+
+    // Iterate from the current column to column 1
+    for (int i = myPosition.getColumn() - 1; i >= 1; i--) {
+      ChessPiece piece = board.getPiece(new ChessPosition(myPosition.getRow(), i));
+      if (piece == null || piece.getTeamColor() != pieceColor) {
+        moves.add(new ChessMove(myPosition, new ChessPosition(myPosition.getRow(), i), null));
+      } else {
+        break; // Stop iteration if blocked by a piece of the opposite color
+      }
+    }
+
+    // Iterate from the current column to column 8
+    for (int i = myPosition.getColumn() + 1; i <= 8; i++) {
+      ChessPiece piece = board.getPiece(new ChessPosition(myPosition.getRow(), i));
+      if (piece == null || piece.getTeamColor() != pieceColor) {
+        moves.add(new ChessMove(myPosition, new ChessPosition(myPosition.getRow(), i), null));
+      } else {
+        break; // Stop iteration if blocked by a piece of the opposite color
+      }
+    }
+
     System.out.println("moves: " + moves);
     return moves;
   }
