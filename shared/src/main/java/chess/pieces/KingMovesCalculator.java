b3a3e07040eb51013be3a4be32172ea37be593a9
Kaydi <@kaydancegillam@gmail.com>
2024-05-04 19:31:59 -0600

    Fully implemented KingMovesCalculator.

--- shared/src/main/java/chess/pieces/KingMovesCalculator.java
+++ shared/src/main/java/chess/pieces/KingMovesCalculator.java
@@ -11,7 +11,20 @@ import java.util.Collection;
 public class KingMovesCalculator {
   public Collection<ChessMove> calculateMoves(ChessBoard board, ChessPosition myPosition, ChessGame.TeamColor pieceColor) {
     var moves = new ArrayList<ChessMove>();
-    moves.add(new ChessMove(myPosition, new ChessPosition(1, 1), null));
+    for (int i = -1; i <= 1; i++) {
+      for (int j = -1; j <= 1; j++) {
+        if (i == 0 && j == 0) {
+          continue;
+        }
+        ChessPosition newPosition = new ChessPosition(myPosition.getRow() + i, myPosition.getColumn() + j);
+        if (newPosition.getRow() >= 1 && newPosition.getRow() <= 8 && newPosition.getColumn() >= 1 && newPosition.getColumn() <= 8) {
+          ChessPiece piece=board.getPiece(newPosition);
+          if (piece == null || piece.getTeamColor() != pieceColor) {
+            moves.add(new ChessMove(myPosition, newPosition, null));
+          }
+        }
+      }
+    }
     return moves;
   }
 }
