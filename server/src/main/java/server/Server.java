bac3c21e6ea3436fd1c459cb9ad700540ed5bc44
Kaydi <@kaydancegillam@gmail.com>
2024-05-21 17:33:25 -0600

    Created model directory in shared module with UserData, GameData, and AuthData records inside.

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -10,13 +10,24 @@ public class Server {
         Spark.staticFiles.location("web");
 
         // Register your endpoints and handle exceptions here.
+        createEndpoints();
+
 
         Spark.awaitInitialization();
+        System.out.println("Server started on port: " + Spark.port());
         return Spark.port();
     }
 
     public void stop() {
         Spark.stop();
         Spark.awaitStop();
+    }
+    private static void createEndpoints() {
+        Spark.get("/hello", (req, res) -> "Hello!");
+    }
+
+    public static void main(String[] args) {
+        Server server = new Server();
+        server.run(8080);
     }
 }
---
+++ shared/src/main/java/model/GameData.java
@@ -0,0 +1,5 @@
+package model;
+
+import chess.ChessGame;
+
+public record GameData(Integer gameID, String whiteUsername, String blackUsername, String gameName, ChessGame game) {}
